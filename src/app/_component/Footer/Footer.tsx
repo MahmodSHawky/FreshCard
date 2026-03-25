6d57c3e53fd980898b56954573f50bc0be69473d
MahmodSHawky <shawkym3334@gmail.com>
2026-03-26 01:28:01 +0200

    feat: initial commit

---
+++ src/app/_component/Footer/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <nav className='bg-blue-500 p-4 text-center fixed bottom-0 inset-e-0 inset-s-0'>
+      Footer
+  </nav>
+  )
+}
